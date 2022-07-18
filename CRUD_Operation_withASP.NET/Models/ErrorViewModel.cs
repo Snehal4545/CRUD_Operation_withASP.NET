e5af570de3f50150f7592a8f075f376cc64fee57
Snehal <pachundkars6@gmail.com>
2022-07-18 17:11:56 +0530

    Add project files.

---
+++ CRUD_Operation_withASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_Operation_withASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUD_Operation_withASP.NET/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Operation_withASP.NET.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Product Name Is Required")]
+        [DataType(DataType.Text)]
+        [Display(Name ="Product Name")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = " Price Is Required")]
+        [Display(Name = "Product Price")]
+        [Range(minimum:1,maximum:50000)]
+        public double Price { get; set; }
+
+
+    }
+}
