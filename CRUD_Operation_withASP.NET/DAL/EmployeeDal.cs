ce945610ccc89be41a1e25f9a1ed5627ca4be6c4
Snehal <pachundkars6@gmail.com>
2022-07-19 14:11:59 +0530

    commit

---
+++ CRUD_Operation_withASP.NET/DAL/EmployeeDal.cs
@@ -0,0 +1,32 @@
+﻿using CRUD_Operation_withASP.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Operation_withASP.NET.DAL
+{
+    public class EmployeeDal
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public EmployeeDal()
+        {
+            con = new SqlConnection(Startup.ConnectionString);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> emp = new List<Employee>();
+            string qry = "select * from Employee";
+            cmd=  
+        }
+        
+    }
+
+    }
+} 
+
---
+++ CRUD_Operation_withASP.NET/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Operation_withASP.NET.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public double  Salary { get; set; }
+
+    }
+}
